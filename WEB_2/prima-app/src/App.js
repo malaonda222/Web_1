09ea7c8c211f1247bb21b60074adf777cdfa3321
malaonda222 <bandinelli.lisa@gmail.com>
2025-06-13 17:08:04 +0200

    commit

--- WEB_2/prima-app/src/App.js
+++ WEB_2/prima-app/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Componente1 from './Componente1';
+import Clock from './Clock';
 
 function App() {
+  let nome="Lisa";
+  const persona1={
+    nome:"Roberto", cognome:"Del", eta:"48"
+  }
+
+  const persona2={
+    nome:"Gino", cognome:"Fim", eta:"35"
+  }
+
+  const data=new Date()
   return (
     <div className="App">
-     <h1>Prima App React</h1>
+      <h1>Prima App React {nome}</h1>
+      <Componente1 {...persona1}/>
+      <Componente1 {...persona2}/>
+      <Componente1 nome="Mario" cognome="Dom" eta="39"></Componente1>
+
+      <h2>
+          {data.toLocaleDateString()+" "+ data.toLocaleTimeString()}
+      </h2>
+      <Clock timezone="-6" country="USA"></Clock>
+      <Clock timezone="0" country="Italy"></Clock>
+      <Clock timezone="7" country="Japan"></Clock>
     </div>
   );
 }
---
+++ WEB_2/prima-app/src/Componente1.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Componente1 = ({ nome, cognome, eta }) => {
+  return (
+    <>
+      <div
+        style={{
+          color: "red",
+          fontWeight: "800",
+          border: "1px #000 solid",
+          margin: "15px",
+          padding: "15px",
+        }}
+      >
+        {nome} {cognome} di anni {eta}
+        <Anagrafica></Anagrafica>
+        <Messaggio></Messaggio>
+      </div>
+      <footer></footer>
+    </>
+  );
+};
+
+const Anagrafica = () => {
+  return <div>Anagrafica</div>;
+};
+
+const Messaggio = () => {
+  return <div>Messaggio</div>;
+};
+
+export default Componente1;
